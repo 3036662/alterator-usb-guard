213650d9c3691c527d74c2da7b89ddb94ed4fd0c
Oleg Proskurin <proskur@altlinux.org>
2025-01-20 12:07:26 +0300

    [fix] log journal pagination  fix jump back (-10)  fix 'empty page at the end' problem

--- backend/dispatcher_impl.cpp
+++ backend/dispatcher_impl.cpp
@@ -75,7 +75,7 @@ bool DispatcherImpl::ReadUsbGuardLogs(const LispMessage &msg) const noexcept {
         common_utils::StrToUint(msg.params.at("page")).value_or(0);
     uint per_page = 5;
     if (msg.params.count("per_page") > 0) {
-      std::cerr << "per page value" << msg.params.at("per_page") << "\n";
+      // std::cerr << "per page value" << msg.params.at("per_page") << "\n";
       per_page = common_utils::StrToUint(msg.params.at("per_page")).value_or(5);
     }
     std::string filter = msg.params.at("filter");
